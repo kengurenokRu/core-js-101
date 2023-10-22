a16bf5f3faf8a2719d41f727e0c4b4a817cb5a3f
kengrenokRu <nastassiadunaeva@gmail.com>
2023-10-22 12:18:55 +0300

    feat: add parst of 4 part

--- src/04-conditions-n-loops-tasks.js
+++ src/04-conditions-n-loops-tasks.js
@@ -27,8 +27,11 @@
  *  21 => 'Fizz'
  *
  */
-function getFizzBuzz(/* num */) {
-  throw new Error('Not implemented');
+function getFizzBuzz(num) {
+  if ((num % 3 === 0) && (num % 5 === 0)) return 'FizzBuzz';
+  if (num % 3 === 0) return 'Fizz';
+  if (num % 5 === 0) return 'Buzz';
+  return num;
 }
 
 
@@ -137,8 +140,16 @@ function isTriangle(a, b, c) {
  *   { top:20, left:20, width: 20, height: 20 }    =>  false
  *
  */
-function doRectanglesOverlap(/* rect1, rect2 */) {
-  throw new Error('Not implemented');
+function doRectanglesOverlap(rect1, rect2) {
+  const nachX2 = rect2.left;
+  const nachY2 = rect2.top;
+  const width1 = rect1.left + rect1.width;
+  const height1 = rect1.top + rect1.height;
+  let fl = false;
+  let fl1 = false;
+  if (height1 >= nachY2) fl = true;
+  if ((width1 >= nachX2)) fl1 = true;
+  return ((fl) && (fl1));
 }
 
 
@@ -168,8 +179,10 @@ function doRectanglesOverlap(/* rect1, rect2 */) {
  *   { center: { x:0, y:0 }, radius:10 },  { x:10, y:10 }   => false
  *
  */
-function isInsideCircle(/* circle, point */) {
-  throw new Error('Not implemented');
+function isInsideCircle(circle, point) {
+  if (Math.sqrt((circle.center.x - point.x) ** 2
+  + (circle.center.y - point.y) ** 2) < circle.radius) return true;
+  return false;
 }
 
 
@@ -211,8 +224,13 @@ function findFirstSingleChar(/* str */) {
  *   5, 3, true, true   => '[3, 5]'
  *
  */
-function getIntervalString(/* a, b, isStartIncluded, isEndIncluded */) {
-  throw new Error('Not implemented');
+function getIntervalString(a, b, isStartIncluded, isEndIncluded) {
+  let str = '';
+  if (isStartIncluded) str = '['; else str = '(';
+  if (a < b) str += `${a}, ${b}`;
+  else str += `${b}, ${a}`;
+  if (isEndIncluded) str += ']'; else str += ')';
+  return str;
 }
 
 
@@ -228,8 +246,10 @@ function getIntervalString(/* a, b, isStartIncluded, isEndIncluded */) {
  * 'rotator' => 'rotator'
  * 'noon' => 'noon'
  */
-function reverseString(/* str */) {
-  throw new Error('Not implemented');
+function reverseString(str) {
+  let newStr = '';
+  for (let i = str.length - 1; i >= 0; i -= 1) newStr += str[i];
+  return newStr;
 }
 
 
@@ -245,8 +265,12 @@ function reverseString(/* str */) {
  *   87354 => 45378
  *   34143 => 34143
  */
-function reverseInteger(/* num */) {
-  throw new Error('Not implemented');
+function reverseInteger(num) {
+  let newStr = '';
+  while (num !== 0) {
+    newStr += num % 10;
+  }
+  return parseInt(newStr, 10);
 }
 
 
